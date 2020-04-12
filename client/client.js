a65c1edb35718b0b6de0432b378abab0bad151fb
unknown <josephgrosenfeld@gmail.com>
2020-04-12 09:36:00 -0400

    Uploading all the shiz for the first time

---
+++ client/client.js
@@ -0,0 +1,68 @@
+
+const form = document.querySelector('form');
+const loadingGif = document.querySelector('.loading');
+const mewsEl = document.querySelector('.mews');
+const API_URL = "http://localhost:5000/mews";
+
+listAllMews();
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+    const mew = {
+        name,
+        content
+    };
+    form.style.display = "none";
+    loadingGif.style.display = "block";
+
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(mew),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })  .then(response => response.json())
+        .then(res => {
+            form.reset();
+            setTimeout(() => {
+            form.style.display = "block";
+            loadingGif.style.display = "none";
+            }, 30000);
+            listAllMews();
+        });
+});
+
+
+
+function listAllMews() {
+    mewsEl.innerHTML = '';
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(mews => {
+            mews.reverse();
+            mews.forEach(mew => {
+                const div = document.createElement('div');
+                div.setAttribute('class', 'mew');
+
+                const header = document.createElement('h3');
+                header.textContent = mew.name;
+
+                const content = document.createElement('p');
+                content.textContent = mew.content;
+
+
+                div.appendChild(header);
+                div.appendChild(content);
+                
+                mewsEl.appendChild(div);
+            });
+        });
+}
+
+
+
+
+
---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const monk = require('monk');
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+
+const db = monk(process.env.MONGO_URI || 'localhost/meower');
+const mews = db.get('mews');
+
+const apiLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1 per minute
+    max: 1
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
+app.get('/mews', (req, res) => {
+    mews
+        .find()
+        .then(mews => {
+            res.json(mews);
+        });
+});
+
+app.use(apiLimiter);
+
+function isValidMew(mew) {
+    return mew.name && mew.name.toString().trim() !== '' &&
+        mew.content && mew.content.toString().trim() !== '';
+}
+
+app.post('/mews', (req, res) => {
+    if(isValidMew(req.body)){
+        console.log(req.body);
+        const mew = {
+            name: req.body.name.toString(),
+            content: req.body.content.toString(),
+            created: new Date()
+        };
+        mews
+            .insert(mew)
+            .then(createdMew => {
+                res.json(createdMew);
+            });
+    } else {
+        res.status(422);
+        res.json({
+            message: 'content and name are required'
+        });
+    }
+});
+
+app.listen(5000, () => {
+    console.log("listening on local host 5000");
+});
